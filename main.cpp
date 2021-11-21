6ddbb669cc574974106e095b12fe7cda38ee2ef0
marshiyar <57055687+marshiyar@users.noreply.github.com>
2021-11-21 14:00:44 -0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+    cout << "My name is Mohammad Rahgozar." << endl;
+    cout << "Today is September 22, 2021" << endl;
+    cout << "6565 + 1341 is " << 6565 + 1341 << endl;
+    cout << "6565 - 1341 is " << 6565 - 1341 << endl;
+    cout << "865 * 32 is " << 865 * 32 << endl;
+    cout << "8756 / 129 is " << 8756 / 129 << endl;
+    cout << "8756 / 129.0 is " << 8756 / 129.0 << endl;
+    
+    
+    /*
+     Homework 1
+     Author : Mohammad Rahgozar
+     Date : 09-27-21
+     Description : The first Homework for the class
+     
+     */
+    return 0;
+}
+
